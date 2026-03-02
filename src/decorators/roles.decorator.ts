24535f6cbd7a054d74643a17b167ebef8f248822
Fernando Campellone <tomascampellone@gmail.com>
2026-03-02 09:57:18 -0300

    feat: new agent logic

--- src/decorators/roles.decorator.ts
+++ src/decorators/roles.decorator.ts
@@ -4,6 +4,8 @@ import { Role } from '../common/enums/role.enum';
 
 export const ROLES_KEY = 'roles';
 export const ALLOW_CLIENT_KEY = 'allowClient';
+export const AGENT_ONLY_KEY = 'agentOnly';
 
 export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
 export const AllowClient = () => SetMetadata(ALLOW_CLIENT_KEY, true);
+export const AgentOnly = () => SetMetadata(AGENT_ONLY_KEY, true);
