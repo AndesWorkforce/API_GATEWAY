2adc9992db54039fe085bf22bc5f4a0e6c74dfb1
David Morcillo <aljodavid100@gmail.com>
2026-03-19 02:30:23 -0500

    fix: url assign

--- src/applications/domains.controller.ts
+++ src/applications/domains.controller.ts
@@ -13,8 +13,7 @@ import { catchError } from 'rxjs';
 
 import { getMessagePattern } from 'config';
 import { Role } from 'src/common/enums/role.enum';
-
-import { AllowClient, AgentOnly, Roles } from 'src/decorators/roles.decorator';
+import { AllowClient, Roles } from 'src/decorators/roles.decorator';
 
 import type { AssignDomainsToContractorDto } from './dto/assign-domains-to-contractor.dto';
 import type { CreateDomainDto } from './dto/create-domain.dto';
@@ -79,7 +78,6 @@ export class DomainsController {
   }
 
   @Roles(Role.Superadmin, Role.TeamAdmin)
-  @AgentOnly()
   @Post('contractor/:contractorId/assign')
   assignDomainsToContractor(
     @Param('contractorId') contractorId: string,
@@ -109,7 +107,6 @@ export class DomainsController {
   }
 
   @Roles(Role.Superadmin, Role.TeamAdmin)
-  @AgentOnly()
   @Delete('contractor/:contractorId/remove')
   removeDomainsFromContractor(
     @Param('contractorId') contractorId: string,
